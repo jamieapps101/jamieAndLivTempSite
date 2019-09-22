83e7557a269c2c4d44aa61bcb268852a8816395d
justletmeliv <oabwebster@gmail.com>
2019-09-22 15:06:21 +0100

    Entered IOT
    Made the arduino connect to the internet + can do a HTTP get request (even when Jamie doesn't have the right security for it to work... :P)

--- ESP32Firmware/ESP32Firmware.ino
+++ ESP32Firmware/ESP32Firmware.ino
@@ -1,21 +1,23 @@
 /*
- * https://circuits4you.com
- * 
- * Connecting DevKit ESP32 to WiFi Example
- * 
- */
- 
-#include <WiFi.h>        // Include the Wi-Fi library
+   https://circuits4you.com
 
-const char* ssid     = "SSID";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "PASSWORD";     // The password of the Wi-Fi network
+   Connecting DevKit ESP32 to WiFi Example
+
+*/
+
+#include <WiFi.h>        // Include the Wi-Fi library
+#include <HTTPClient.h>
+#define USE_SERIAL Serial
+
+const char* ssid     = "ASK4 Wireless";         // The SSID (name) of the Wi-Fi network you want to connect to
+//const char* password = "PASSWORD";     // The password of the Wi-Fi network
 
 void setup() {
   Serial.begin(115200);         // Start the Serial communication to send messages to the computer
   delay(10);
   Serial.println('\n');
-  
-  WiFi.begin(ssid, password);             // Connect to the network
+
+  WiFi.begin(ssid);             // Connect to the network
   Serial.print("Connecting to ");
   Serial.print(ssid);
 
@@ -25,16 +27,43 @@ void setup() {
   }
 
   Serial.println('\n');
-  Serial.println("Connection established!");  
+  Serial.println("Connection established!");
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
 
   delay(3000);
-  Serial.println(WiFi.ping("jamieapps.net");
+  //  Serial.println(WiFi.ping("jamieapps.net");
   delay(1000);
+
+  HTTPClient http;
+
+  USE_SERIAL.print("[HTTP] begin...\n");
+  // configure traged server and url
+  //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
+  http.begin("http://jamieapps.net/index.nginx-debian.html"); //HTTP
+
+  USE_SERIAL.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      USE_SERIAL.println(payload);
+    }
+  } else {
+    USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
 }
 
-void loop() 
-{ 
-  
+void loop()
+{
+
 }
