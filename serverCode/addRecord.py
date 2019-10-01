6013f5dc7b585d365917f6fea640ab3f75ca5dbd
jamie <jamieapps101@gmail.com>
2019-10-01 08:24:22 +0100

    some progress to addRecord.py
    more just to transfer to server really

--- serverCode/addRecord.py
+++ serverCode/addRecord.py
@@ -1,33 +1,55 @@
 #! /usr/bin/python3
-
-
-# enable debugging
 import cgitb
-cgitb.enable()
-
-#print("Content-Type: text/plain;charset=utf-8")
-#print()
-
-#print("Hello World!")
-
-
+cgitb.enable() # enable debugging
 import json
 import pprint
 import urllib
-
+import mysql.connector
+import time
+import datetime
 
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html'),("Access-Control-Allow-Origin","*")])
+    start_response('200 OK', [('Content-Type','application/json'))])
     #pprint.pprint(env)
     #print("I got: \n{}\n".format(env["QUERY_STRING"]))
     data = dict(urllib.parse.parse_qsl(env["QUERY_STRING"]))
     for key in data:
         try:
-            data[key] = int(data[key])
+            data[key] = float(data[key])
         except:
             print(data)
             exit()
-    result = {"result":data["a"] + data["b"]}
-    yield json.dumps(result).encode("utf-8")
-    #yield str("hello").encode('utf-8')
+    success = True
+    unixTime = time.time()
+    dateString = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    SQLdata = [0.0,0.0,0.0]
+    feilds = ["temperature","humidity","pressure"]
+    gotFeilds = all([field in data for field in fields])
+    success = gotFeilds
+    if len(data) != 3:
+        success = False
 
+    if success == True:
+        for index in range(3):
+            key = fields[index]
+            SQLdata[index] = data[key]
+
+    # do sql operations here
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="applicationUser",
+        passwd="applicationPassword",
+        database = "sensorData"
+    )
+    dbcursor = mydb.cursor()
+
+    command = "" # need to craft a command
+
+    dbcursor.execute(command)
+
+
+    if(success == True):
+        yield json.dumps({"Result":"Success"}).encode("utf-8")
+    else:
+        yield json.dumps({"Result":"Fail"}).encode("utf-8")
+    #yield str("hello").encode('utf-8')
