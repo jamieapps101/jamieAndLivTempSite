fada9ca3fa8e35bdf1c5f9721df73c5a4a349ac8
jamie apps <jamie@ubuntu18.04.3>
2019-10-03 13:30:47 +0000

    server code all working!
    pls don't ever touch this again...

--- serverCode/apps/receiveData/addRecord.py
+++ serverCode/apps/receiveData/addRecord.py
@@ -14,44 +14,46 @@ import datetime
 def application(env, start_response):
     #start_response('200 OK', [('Content-Type','application/json')])
     start_response('200 OK', [('Content-Type','text/html')])
+    try:
+        data = dict(urllib.parse.parse_qsl(env["QUERY_STRING"]))
 
-    #pprint.pprint(env)
-    #print("I got: \n{}\n".format(env["QUERY_STRING"]))
-    data = dict(urllib.parse.parse_qsl(env["QUERY_STRING"]))
+        for key in data:
+            try:
+                data[key] = float(data[key])
+            except:
+                print(data)
+                return json.dumps({"Result":"Fail"}).encode("utf-8")
 
-    for key in data:
-        try:
-            data[key] = float(data[key])
-        except:
-            print(data)
-            return json.dumps({"Result":"Fail"}).encode("utf-8")
-
-    success = True
-    unixTime = time.time()
-    now = datetime.datetime.now()
-    dateString = now.strftime("%H:%M:%S %d/%m/%Y")
-    SQLdata = [0.0,0.0,0.0]
-    fields = ["temperature","humidity","pressure"]
-    if len(data) == 3 and all([field in data for field in fields]):
-        for index in range(3):
-            key = fields[index]
-            SQLdata[index] = data[key]
+        fields = ["temperature","humidity","pressure"]
+        if len(data) == 3 and all([field in data for field in fields]):
+            unixTime = time.time()
+            now = datetime.datetime.now()
+            dateString = now.strftime("%H:%M:%S %d/%m/%Y")
+            SQLdata = [0.0,0.0,0.0]
+            for index in range(3):
+                key = fields[index]
+                SQLdata[index] = data[key]
         # do sql operations here
-        mydb = mysql.connector.connect(
-           host="localhost",
-           user="applicationUser",
-           passwd="applicationPassword",
-           database = "sensorData"
-        )
-        dbcursor = mydb.cursor()
-        command = "INSERT INTO log VALUES (" # need to craft a command
-        command += unixTime + ", "
-        command += dateString + ", "
-        command += SQLdata[0] + ", "
-        command += SQLdata[1] + ", "
-        command += SQLdata[2] + ");"
-        dbcursor.execute(command)
-        yield json.dumps({"Result":"Success"}).encode("utf-8")
-    else:
-       yield json.dumps({"Result":"Fail"}).encode("utf-8")
-    yield str("You tried to add data").encode('utf-8')
+            mydb = mysql.connector.connect(
+               host="localhost",
+               user="applicationUser",
+               passwd="applicationPassword",
+               database = "sensorData"
+            )
+            dbcursor = mydb.cursor()
+            command = "INSERT INTO log (unixTime,time,temperature,humidity,preasure) VALUES (" # need to craft a command
+            command += str(unixTime) + ", "
+            command += "\"" + dateString + "\", "
+            command += str(SQLdata[0]) + ", "
+            command += str(SQLdata[1]) + ", "
+            command += str(SQLdata[2]) + ");"
+            #yield ("Command: " + command).encode('utf-8')
+            dbcursor.execute(command)
+            mydb.commit()
+            yield json.dumps({"Result":"success"}).encode("utf-8")
+        else:
+           yield json.dumps({"Result":"fail","meta": "from addRecord.py"}).encode("utf-8")
+    except Exception as e:
+    	#yield str("You tried to add data").encode('utf-8')
+        yield str(e).encode('utf-8')
+	#yield json.dumps({"Result":"fail","meta": "from addRecord, script failure"}).encode("utf-8")
--- serverCode/apps/sendData/getRecords.py
+++ serverCode/apps/sendData/getRecords.py
@@ -1,14 +1,8 @@
 #! /usr/bin/python3
 
 
-# enable debugging
 import cgitb
 cgitb.enable()
-
-#print("Content-Type: text/plain;charset=utf-8")
-#print()
-
-#print("Hello World!")
 
 
 import json
@@ -18,34 +12,34 @@ import mysql.connector
 
 
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html'),("Access-Control-Allow-Origin","*")])
-    pprint.pprint(env)
-    print("I got: \n{}\n".format(env["QUERY_STRING"]))
-    data = dict(urllib.parse.parse_qsl(env["QUERY_STRING"]))
-    success = True
-    for key in data:
-       try:
-           data[key] = int(data[key])
-       except:
-           print(data)
-           success = False
+    #start_response('200 OK', [('Content-Type','application/json')])
+    start_response('200 OK', [('Content-Type','text/html')])
+    try:
+        # yield str("A").encode('utf-8')
+        data = dict(urllib.parse.parse_qsl(env["QUERY_STRING"]))
+        # yield str("B").encode('utf-8')
+        if ("records" in data) and (len(data)==1):
+            # yield str("C").encode('utf-8')
+            recordsRequest = int(data["records"])
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user="applicationUser",
+                passwd="applicationPassword",
+                database = "sensorData"
+            )
+            # yield str("D").encode('utf-8')
+            dbcursor = mydb.cursor()
+            command = "SELECT * FROM log ORDER BY id DESC LIMIT " + str(recordsRequest)
+            dbcursor.execute(command)
+            # yield str("E").encode('utf-8')
+            requestedData = dbcursor.fetchall()
+            jsonData = json.dumps(requestedData)
+            returnData = {"result":"success","data":jsonData}
+            # yield str("F").encode('utf-8')
+            yield json.dumps(returnData).encode("utf-8")
+        else:
+            returnData = {"result":"fail","meta":"from getRecords.py"}
+            yield json.dumps(returnData).encode("utf-8")
 
-    recordsRequest = int(data["records"])
-    mydb = mysql.connector.connect(
-       host="localhost",
-       user="applicationUser",
-       passwd="applicationPassword",
-       database = "sensorData"
-    )
-    dbcursor = mydb.cursor()
-
-    command = "SELECT * FROM log ORDER BY id DESC LIMIT " + str(recordsRequest)
-
-    dbcursor.execute(command)
-
-    requestedData = dbcursor.fetchall()
-
-    jsonData = json.dumps(requestedData)
-
-    yield jsonData.encode("utf-8")
-    yield str("You tried to get data").encode('utf-8')
+    except Exception as e:
+        return json.dumps({"Result":"fail","meta":str(e)}).encode("utf-8")
